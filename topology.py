c3f32b2ba5168fd418495520b0f513b6fb352326
wings-Q <im.qipf@gmail.com>
2019-12-06 21:15:26 +0800

    topology.py

--- topology.py
+++ topology.py
@@ -1,2 +1,16 @@
 from fem2d import *
+import numpy as n
+
+def cmat(delta,ke):
+    return n.dot(n.dot(delta.T,ke),delta)
+
+
+
+nx = 40
+ny = 40
+
+m = Mesh(nx,ny)
+nodes,element2Ds = m.create()
+
+s = System(nodes,element2Ds)
 
