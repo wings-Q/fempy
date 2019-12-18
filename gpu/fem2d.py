5a85a20ecb60caed9e6cbc5d43a5d19d9909d67d
wings-Q <im.qipf@gmail.com>
2019-12-18 13:49:45 +0800

    autopep

--- gpu/fem2d.py
+++ gpu/fem2d.py
@@ -3,13 +3,14 @@ import matplotlib.pyplot as plt
 
 
 class Node(object):
-    def __init__(self,ID,position,load=None):
+    def __init__(self, ID, position, load=None):
         self.position = position
         self.ID = ID
         self.load = load
 
+
 class element2D(object):
-    def __init__(self,nodes,t,v,E):
+    def __init__(self, nodes, t, v, E):
         self.nodes = nodes
         self.v = v
         self.t = t
@@ -25,33 +26,34 @@ class element2D(object):
         s = (1-3*self.v)/8
         H = 1/(1-self.v**2)
         r = (1-self.v)/2
-        k = [al+r*be,m,r*al/2-be,-s,-be/2-r*al/2,-m,be/2-r*al,s,be+r*al,al/2-r*be,al/2-r*be/2,r*be/2-al,-al/2-r*be/2,r*al/2-be]
-        ke = n.asarray([[k[8],k[1],k[2],k[3],k[4],k[5],k[6],k[7]],\
-                        [k[1],k[0],k[7],k[9],k[5],k[10],k[3],k[11]],\
-                        [k[2],k[7],k[8],k[5],k[6],k[3],k[4],k[1]],\
-                        [k[3],k[9],k[5],k[0],k[7],k[11],k[1],k[12]],\
-                        [k[4],k[5],k[6],k[7],k[8],k[1],k[13],k[3]],\
-                        [k[5],k[10],k[3],k[11],k[1],k[0],k[7],k[9]],\
-                        [k[6],k[3],k[4],k[1],k[13],k[7],k[8],k[5]],\
-                        [k[7],k[11],k[1],k[12],k[3],k[9],k[5],k[0]]])
+        k = [al+r*be, m, r*al/2-be, -s, -be/2-r*al/2, -m, be/2-r*al, s, be +
+             r*al, al/2-r*be, al/2-r*be/2, r*be/2-al, -al/2-r*be/2, r*al/2-be]
+        ke = n.asarray([[k[8], k[1], k[2], k[3], k[4], k[5], k[6], k[7]],
+                        [k[1], k[0], k[7], k[9], k[5], k[10], k[3], k[11]],
+                        [k[2], k[7], k[8], k[5], k[6], k[3], k[4], k[1]],
+                        [k[3], k[9], k[5], k[0], k[7], k[11], k[1], k[12]],
+                        [k[4], k[5], k[6], k[7], k[8], k[1], k[13], k[3]],
+                        [k[5], k[10], k[3], k[11], k[1], k[0], k[7], k[9]],
+                        [k[6], k[3], k[4], k[1], k[13], k[7], k[8], k[5]],
+                        [k[7], k[11], k[1], k[12], k[3], k[9], k[5], k[0]]])
 
-        
         return (self.E*H*self.t)*ke
-        
+
     def ID(self):
         n1id = self.nodes[0].ID
         n2id = self.nodes[1].ID
         n3id = self.nodes[2].ID
         n4id = self.nodes[3].ID
-        return [n1id,n2id,n3id,n4id]
+        return [n1id, n2id, n3id, n4id]
+
 
 class System(object):
-    def __init__(self,nodes,element2Ds):
+    def __init__(self, nodes, element2Ds):
         self.nodes = nodes
         self.el2ds = element2Ds
 
     def KE(self):
-        ke = n.zeros([len(self.nodes)*2,len(self.nodes)*2])
+        ke = n.zeros([len(self.nodes)*2, len(self.nodes)*2])
         for el2d in self.el2ds:
             elke = el2d.KE()
             elids = el2d.ID()
@@ -61,12 +63,13 @@ class System(object):
                 elu.append(2*elid+1)
             for i in range(8):
                 for j in range(8):
-                    ke[elu[i],elu[j]] = ke[elu[i],elu[j]] + elke[i,j]
+                    ke[elu[i], elu[j]] = ke[elu[i], elu[j]] + elke[i, j]
         return ke
+
     def solve(self):
         k1 = self.KE()
         k0 = self.KE()
-        P = n.zeros([2*len(self.nodes),1])
+        P = n.zeros([2*len(self.nodes), 1])
         for node in self.nodes:
             load = node.load
             i = node.ID
@@ -77,41 +80,43 @@ class System(object):
             forcex = load[1][0]
             forcey = load[1][1]
             if tranx != None:
-                k1[2*i,2*i] = k1[2*i,2*i]*(10**8)
-                P[2*i] = k1[2*i,2*i]*tranx
+                k1[2*i, 2*i] = k1[2*i, 2*i]*(10**8)
+                P[2*i] = k1[2*i, 2*i]*tranx
             if trany != None:
-                k1[2*i+1,2*i+1] = k1[2*i+1,2*i+1]*(10**8)
-                P[2*i+1] = k1[2*i+1,2*i+1]*trany
+                k1[2*i+1, 2*i+1] = k1[2*i+1, 2*i+1]*(10**8)
+                P[2*i+1] = k1[2*i+1, 2*i+1]*trany
             P[2*i] = P[2*i]+forcex
             P[2*i+1] = P[2*i+1]+forcey
-        delta = n.linalg.solve(k1,P)
-        Force = n.dot(k0,delta)
-        return delta,Force
+        delta = n.linalg.solve(k1, P)
+        Force = n.dot(k0, delta)
+        return delta, Force
 
-    def dc(self,h,dens):
+    def dc(self, h, dens):
         dcs = []
-        delta,force = self.solve()
-        for i,el2d in enumerate(self.el2ds):
+        delta, force = self.solve()
+        for i, el2d in enumerate(self.el2ds):
             eldelta = []
             for elid in el2d.ID():
                 eldelta.append(delta[2*elid])
                 eldelta.append(delta[2*elid+1])
             eldelta = n.asarray(eldelta)
-            dc = h*(dens[i])**(h-1)*n.dot(eldelta.T,n.dot(el2d.KE(),eldelta))
+            dc = h*(dens[i])**(h-1)*n.dot(eldelta.T, n.dot(el2d.KE(), eldelta))
             dcs.append(dc[0][0])
         return dcs
 
-    def load(self,loads):
+    def load(self, loads):
         for load in loads:
             self.nodes[load['nodeID']].load = load['load']
 
+
 class Mesh(object):
-    def __init__(self,nx,ny,a,b):
+    def __init__(self, nx, ny, a, b):
         self.nx = nx
         self.ny = ny
         self.a = a
         self.b = b
-    def create(self,E):
+
+    def create(self, E):
         nodes = []
         element2Ds = []
         k = 0
@@ -119,7 +124,7 @@ class Mesh(object):
             positionx = i*2*self.a
             for j in range(self.ny):
                 positiony = j*2*self.b
-                nodes.append(Node(k,[positionx,positiony]))
+                nodes.append(Node(k, [positionx, positiony]))
                 k = k+1
         #nodes[len(nodes)-self.ny//2].load = [[None,None],[0,1]]
         for i in range(self.nx-1):
@@ -128,64 +133,65 @@ class Mesh(object):
                 n4 = nodes[i*self.ny+1+j]
                 n2 = nodes[i*self.ny+j+self.ny]
                 n3 = nodes[i*self.ny+j+self.ny+1]
-                #print(i*(ny-1)+j)
+                # print(i*(ny-1)+j)
                 e = E[i*(self.ny-1)+j]
-                #print(e)
-                
-                element2Ds.append(element2D([n1,n2,n3,n4],0.1,0.2,e))
-        return nodes,element2Ds
-    
-    def changeE(self,element2Ds,E):
-        for n,el2d in enumerate(element2Ds):
+                # print(e)
+
+                element2Ds.append(element2D([n1, n2, n3, n4], 0.1, 0.2, e))
+        return nodes, element2Ds
+
+    def changeE(self, element2Ds, E):
+        for n, el2d in enumerate(element2Ds):
             el2d.E = E[n]
         return element2Ds
 
-    
-#print(element2Ds[1].ID())
+
+# print(element2Ds[1].ID())
 #nodes[len(nodes)-1].load = [[None,None],[0,1]]
 #nodes[0].load = [[0,0],[0,0]]
 
 if __name__ == '__main__':
-    
+
     # Disable memory pool for device memory (GPU)
     n.cuda.set_allocator(None)
 
     # Disable memory pool for pinned memory (CPU).
     n.cuda.set_pinned_memory_allocator(None)
-#print(element2Ds[1].ID())
+# print(element2Ds[1].ID())
     nx = 120
     ny = 60
     elenum = (nx-1)*(ny-1)
-    m = Mesh(nx,ny,1,1)
+    m = Mesh(nx, ny, 1, 1)
     E = 100000*n.ones(elenum)
-    nodes,element2Ds = m.create(E)
-    s = System(nodes,element2Ds)
-    loads1 = [{'nodeID':ny-1,'load':[[0,0],[0,0]]},{'nodeID':nx*ny-1,'load':[[0,0],[0,0]]},{'nodeID':ny*(nx//2),'load':[[None,None],[0,-2]]},{'nodeID':ny*(nx//2-1),'load':[[None,None],[0,-2]]}]
-    loads2 = [{'nodeID':nx*ny-ny//3-1,'load':[[0,0],[0,0]]}]
+    nodes, element2Ds = m.create(E)
+    s = System(nodes, element2Ds)
+    loads1 = [{'nodeID': ny-1, 'load': [[0, 0], [0, 0]]}, {'nodeID': nx*ny-1, 'load': [[0, 0], [0, 0]]},
+              {'nodeID': ny*(nx//2), 'load': [[None, None], [0, -2]]}, {'nodeID': ny*(nx//2-1), 'load': [[None, None], [0, -2]]}]
+    loads2 = [{'nodeID': nx*ny-ny//3-1, 'load': [[0, 0], [0, 0]]}]
     for i in range(nx-1):
-        loads2.append({'nodeID':i,'load':[[None,None],[-2,0]]})
+        loads2.append({'nodeID': i, 'load': [[None, None], [-2, 0]]})
     s.load(loads1)
-    delta,force = s.solve()
+    delta, force = s.solve()
     deltay = n.abs(delta[1::2])
-    deltayimage = deltay.reshape((nx,ny)).T
+    deltayimage = deltay.reshape((nx, ny)).T
     deltayimage = n.asnumpy(deltayimage)
     plt.imshow(deltayimage)
     plt.savefig("tmp\\deltay.png")
 
     forcey = n.abs(force[1::2])
-    forceyimage = forcey.reshape((nx,ny)).T    
+    forceyimage = forcey.reshape((nx, ny)).T
     forceyimage = n.asnumpy(forceyimage)
     plt.imshow(forceyimage)
     plt.savefig("tmp\\forcey.png")
 
     deltax = n.abs(delta[0::2])
-    deltaximage = deltax.reshape((nx,ny)).T    
+    deltaximage = deltax.reshape((nx, ny)).T
     deltaximage = n.asnumpy(deltaximage)
     plt.imshow(deltaximage)
     plt.savefig("tmp\\deltax.png")
 
     forcex = n.abs(force[0::2])
-    forceximage = forcex.reshape((nx,ny)).T    
+    forceximage = forcex.reshape((nx, ny)).T
     forceximage = n.asnumpy(forceximage)
     plt.imshow(forceximage)
     plt.savefig("tmp\\forcex.png")
--- gpu/topology.py
+++ gpu/topology.py
@@ -1,11 +1,11 @@
 from fem2d import *
 import cupy as cp
 import math
-    
-    # Disable memory pool for device memory (GPU)
+
+# Disable memory pool for device memory (GPU)
 cp.cuda.set_allocator(None)
 
-    # Disable memory pool for pinned memory (CPU).
+# Disable memory pool for pinned memory (CPU).
 cp.cuda.set_pinned_memory_allocator(None)
 
 
@@ -13,46 +13,49 @@ nx = 81
 ny = 81
 den = 0.2
 h = 5
-m = Mesh(nx,ny,1,1)
-loads1 = [{'nodeID':nx-1,'load':[[None,0],[0,0]]},{'nodeID':nx*ny-1,'load':[[None,0],[0,0]]},{'nodeID':ny*(nx//3)-1,'load':[[None,None],[0,2]]}]
-loads1 = [{'nodeID':nx-1,'load':[[None,0],[0,0]]},{'nodeID':nx*ny-1,'load':[[None,0],[0,0]]},{'nodeID':ny*(nx//3*2)-1,'load':[[None,None],[0,2]]}]
+m = Mesh(nx, ny, 1, 1)
+loads1 = [{'nodeID': nx-1, 'load': [[None, 0], [0, 0]]}, {'nodeID': nx*ny-1, 'load': [[None, 0], [0, 0]]}, {'nodeID': ny*(nx//3)-1, 'load': [[None, None], [0, 2]]}]
+loads2 = [{'nodeID': nx-1, 'load': [[None, 0], [0, 0]]}, {'nodeID': nx*ny-1, 'load': [[None, 0], [0, 0]]}, {'nodeID': ny*(nx//3*2)-1, 'load': [[None, None], [0, 2]]}]
 elenum = (nx-1)*(ny-1)
-dens = cp.full(elenum,den)
+dens = cp.full(elenum, den)
 E = dens**h
-nodes,element2Ds = m.create(E)
+nodes, element2Ds = m.create(E)
 
-def OC(nx,ny,dens,den,dc):
+
+def OC(nx, ny, dens, den, dc):
     l1 = 0
     l2 = 100000
     move = 0.2
     while(l2-l1 > 10**(-4)):
         lmid = 0.5*(l2+l1)
         densnew = []
-        for n,densipy in enumerate(dens):
-            #print(dc[n])
-            dennew = max((0.001,max(densipy-move,min(1.0,min(densipy+move,densipy*math.sqrt(-dc[n]/lmid))))))            
+        for n, densipy in enumerate(dens):
+            # print(dc[n])
+            dennew = max((0.001, max(
+                densipy-move, min(1.0, min(densipy+move, densipy*math.sqrt(-dc[n]/lmid))))))
             if isinstance(dennew, float):
                 pass
             else:
                 dennew = dennew.tolist()
-            #print(type(dennew))
+            # print(type(dennew))
             densnew.append(dennew)
         if sum(densnew) - den*nx*ny > 0:
             l1 = lmid
         else:
             l2 = lmid
-    #print(densnew)
+    # print(densnew)
     return cp.asarray(densnew)
 
+
 for i in range(30):
-    #print(dens)
+    # print(dens)
     E = dens**h
-    element2Ds = m.changeE(element2Ds,E)
-    s = System(nodes,element2Ds)
-    dc = s.dc(h,dens)
-    densnew = OC(nx,ny,dens,den,dc)
+    element2Ds = m.changeE(element2Ds, E)
+    s = System(nodes, element2Ds)
+    dc = s.dc(h, dens)
+    densnew = OC(nx, ny, dens, den, dc)
     densimage = cp.asarray(densnew)
-    denimage = densimage.reshape((nx-1,ny-1)).T
+    denimage = densimage.reshape((nx-1, ny-1)).T
     denimage = cp.asnumpy(denimage)
     plt.imshow(denimage)
     filename = "gpu\\tmp\\denimage"+str(i+1)+".png"
@@ -60,5 +63,3 @@ for i in range(30):
     if cp.max(cp.abs(dens-densnew)).tolist() < 0.001:
         break
     dens = densnew
-
-
